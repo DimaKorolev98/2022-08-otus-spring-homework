43a20c793749783d2e5281e273be20db9c673d51
DimaKorolev98 <112580915+DimaKorolev98@users.noreply.github.com>
2022-12-11 19:22:52 +0300

    Delete src/main/java/ru/otus/homework/service directory

--- src/main/java/ru/otus/homework/service/LocaleService.java
+++
@@ -1,9 +0,0 @@
-package ru.otus.homework.service;
-
-import java.util.Locale;
-
-public interface LocaleService {
-    void defineLocale(Locale locale);
-
-    Locale getLocaleForSession();
-}
--- src/main/java/ru/otus/homework/service/StudentService.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.homework.service;
-
-import ru.otus.homework.domain.Student;
-
-public interface StudentService {
-    Student getStudent();
-}
